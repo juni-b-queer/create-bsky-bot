f21a04d9c3aa78f272a047fba0d7f06ae02f7672
Juni! <154621244+juni-b-queer@users.noreply.github.com>
2024-12-10 16:29:36 -0600

    major: Release 2.0.0 (#2)
    * update for latest beta
    
    * feature: Added additional templates and gh action
    
    * minor: update versions and remove comments
    
    * major: Update to 2.0.0-beta.19
    
    * patch: update dependencies
    
    * update to 2.0.0

---
+++ templates/interval/README.md
@@ -0,0 +1,15 @@
+# {{name}}
+
+{{description}}
+
+# Quickstart
+- [ ] Install Bun
+- [ ] Install Docker
+- [ ] Fill in .env with your handle and **app** password (do not use your real password)
+- [ ] Run `make up`
+- [ ] Be free and create to your hearts content!
+
+
+## Development
+
+Check out the bsky-event-handler docs [here](https://github.com/juni-b-queer/bsky-event-handlers)
--- templates/interval/src/index.ts
+++ templates/interval/src/index.ts
@@ -1,14 +1,6 @@
 import {
-    BadBotHandler,
-    CreateSkeetHandler,
     DebugLog,
-    GoodBotHandler,
     HandlerAgent,
-    InputEqualsValidator,
-    JetstreamSubscription,
-    LogMessageAction,
-    ReplyingToBotValidator,
-    MessageHandler,
     IntervalSubscription,
     IntervalSubscriptionHandlers,
     AbstractHandler,
@@ -21,25 +13,6 @@ const testAgent = new HandlerAgent(
     <string>Bun.env.TEST_BSKY_HANDLE,
     <string>Bun.env.TEST_BSKY_PASSWORD
 );
-
-/**
- * Jetstream Subscription setup
- */
-let jetstreamSubscription: JetstreamSubscription;
-
-let handlers = {
-    post: {
-        c: [
-            new MessageHandler(
-                [ReplyingToBotValidator().make(), InputEqualsValidator("Hello").make()],
-                [LogMessageAction().make(), CreateSkeetAction.make('World!', MessageHandler.generateReplyFromMessage)],
-                testAgent
-            ),
-            new GoodBotHandler(testAgent),
-            new BadBotHandler(testAgent)
-        ]
-    }
-}
 
 let intervalSubscription: IntervalSubscription;
 
@@ -58,17 +31,12 @@ const intervalSubscriptionHandlers: IntervalSubscriptionHandlers = [
 
 async function initialize() {
     await testAgent.authenticate()
-    jetstreamSubscription = new JetstreamSubscription(
-        handlers,
-        <string>Bun.env.JETSTREAM_URL
-    );
     intervalSubscription = new IntervalSubscription(
         intervalSubscriptionHandlers
     )
 }
 
 initialize().then(() =>{
-    jetstreamSubscription.createSubscription()
     intervalSubscription.createSubscription()
     DebugLog.info("INIT", 'Initialized!')
 });
